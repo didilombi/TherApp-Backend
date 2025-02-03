e70bcad24cdc718326cc105cb0a32bb584daaf5f
Miguel Garcia <miguel.garciabaeza@iesdoctorbalmis.com>
2025-02-03 09:04:18 +0100

    ajustes

--- src/main/java/com/therapp/spring/SaludMentalAppApplication.java
+++ src/main/java/com/therapp/spring/SaludMentalAppApplication.java
@@ -1,77 +1,59 @@
 package com.therapp.spring;
 
-import java.time.LocalDate;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.therapp.spring.modelo.Publicacion;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import com.therapp.spring.modelo.Rol;
-import com.therapp.spring.modelo.RolPublicacion;
 import com.therapp.spring.modelo.Usuario;
-import com.therapp.spring.servicios.PublicacionService;
+import com.therapp.spring.servicios.TerapeutaService;
 import com.therapp.spring.servicios.UsuarioService;
+
+import jakarta.transaction.Transactional;
+
+import com.therapp.spring.modelo.Terapeuta;
 
 @SpringBootApplication
 public class SaludMentalAppApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SaludMentalAppApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SaludMentalAppApplication.class, args);
+	}
 
-    @Bean
-    @Transactional
-    CommandLineRunner initData(UsuarioService usuarioService, PublicacionService publicacionService) {
-        return args -> {
-            // Crear un nuevo usuario
-            Usuario usuario1 = new Usuario(
-                "Carlos", 
-                "CarlosOrg", 
-                "carlos@org.com", 
-                "password", 
-                "Sin Imagen", 
-                Rol.ORGANIZACION, 
-                "87654321X", 
-                LocalDate.of(1985, 5, 15), 
-                "123456789", 
-                "Madrid", 
-                "Biografia de Carlos"
-            );
+	@Bean
+	@Transactional
+	CommandLineRunner initData(UsuarioService usuarioService,TerapeutaService terapeutaService) {
+		return args -> {
 
-            Usuario usuario2 = new Usuario(
-                "Ana", 
-                "AnaColab", 
-                "ana@colab.com", 
-                "password", 
-                "Sin Imagen", 
-                Rol.USUARIO, 
-                "12345678X", 
-                LocalDate.of(1990, 8, 20), 
-                "987654321", 
-                "Barcelona", 
-                "Biografia de Ana"
-            );
+			// Lista de usuarios iniciales que quiero guardar en la base de datos
+			List<Usuario> usuarios = Arrays.asList(
+				new Terapeuta("Juan", "JuanIncognito","juan@gmail.com","1234","Sin Imagen",Rol.TERAPEUTA,"12345678A",LocalDate.of(1989, 12, 12), "132456789", "Alicante","Biografia Usuario", "123ABC","Apellidos usuario", "Experiencia en terapia", "Especialidad X", "Español, Inglés"),
+				new Usuario("Maria", "MariaIncognito","maria@gmail.com","1234","Sin Imagen",
+							Rol.ADMIN,"12345678B",LocalDate.of(1989, 12, 12),
+							"132456789","Alicante","Biografia Usuario"),
+				new Usuario("Paco", "PacoIncognito","paco@gmail.com","1234","Sin Imagen",
+							Rol.ORGANIZACION,"12345678C",LocalDate.of(1989, 12, 12),
+							"132456789","Alicante","Biografia Usuario")
+			);
 
-            // Guardar los usuarios en la base de datos
-            usuarioService.save(usuario1);
-            usuarioService.save(usuario2);
+			usuarioService.saveAll(usuarios); //Guardar todos los usuarios en la base de datos
 
-            // Crear una nueva publicación
-            Publicacion publicacion = new Publicacion();
-            publicacion.setTexto("Esta es una nueva publicación");
-            publicacion.setFechaPublicacion("2023-10-01");
+			
+			/* 
+				LOS CAMBIOS QUE HIZO SERGIO
+			
+			
+			Usuario usuarionuevo = usuarioService.findByEmail("paco@gmail.com");
+			usuarioService.transform(usuarionuevo);
+			Terapeuta t = new Terapeuta(usuarionuevo,"numero de colegiado nuevo","Apellido nuevo", "Experiencia en terapia nueva", "Especialidad X nueva", "Español, Inglés nuevo");
+			usuarioService.delete(usuarionuevo);
+			usuarioService.save(t);
+			*/
 
-            // Guardar la publicación en la base de datos
-            publicacion = publicacionService.guardarPublicacion(publicacion);
+			};
+		};
 
-            // Guardar la relación con el usuario como autor
-            publicacionService.crearPublicacion(usuario1, publicacion, RolPublicacion.AUTOR);
-
-            // Añadir un colaborador a la publicación
-            publicacionService.crearPublicacion(usuario2, publicacion, RolPublicacion.COLABORADOR);
-        };
-    }
-}
+	}
