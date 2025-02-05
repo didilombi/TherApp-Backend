589eceb0bac03d47c730b595b171e4b51f1ba7ad
miggarbae <145559346+miggarbae@users.noreply.github.com>
2025-02-05 16:18:50 +0100

    cambios para el web socket

--- src/main/java/com/therapp/spring/controladores/ChatWebSocketController.java
+++ src/main/java/com/therapp/spring/controladores/ChatWebSocketController.java
@@ -5,7 +5,6 @@ import com.therapp.spring.modelo.Mensaje;
 import com.therapp.spring.servicios.MensajeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
