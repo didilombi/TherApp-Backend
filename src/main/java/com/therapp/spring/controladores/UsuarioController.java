4c0e0ba75667f631a664b37e0da7c5a420ed6fa1
Miguel Garcia <miguel.garciabaeza@iesdoctorbalmis.com>
2025-02-10 14:38:39 +0100

    cambiando utimas cosas del registro para cambiar fotos y pefil

--- src/main/java/com/therapp/spring/controladores/UsuarioController.java
+++ src/main/java/com/therapp/spring/controladores/UsuarioController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.MediaType;
 import org.springframework.http.MediaType;
 import com.therapp.spring.modelo.Rol;
 import com.therapp.spring.modelo.Usuario;
@@ -67,6 +70,7 @@ public class UsuarioController {
         @RequestParam("foto") MultipartFile foto
     ) {
         try {
+            // Llamamos al servicio para guardar la foto
             usuarioService.guardarFoto(id, foto);
             return ResponseEntity.ok("Foto subida correctamente.");
         } catch (Exception e) {
