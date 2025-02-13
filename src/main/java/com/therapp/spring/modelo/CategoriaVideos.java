25f3c8a9339f1f4fd01b44534400d6e823d7aaba
David Ramirez <david.ramirez@iesdoctorbalmis.com>
2025-02-13 14:59:59 +0100

    videos y categorias creados

---
+++ src/main/java/com/therapp/spring/modelo/CategoriaVideos.java
@@ -0,0 +1,26 @@
+package com.therapp.spring.modelo;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CategoriaVideos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+
+    @ManyToMany(mappedBy = "categorias")
+    private Set<Video> videos;
+}
---
+++ src/main/java/com/therapp/spring/servicios/CategoriaService.java
@@ -0,0 +1,18 @@
+package com.therapp.spring.servicios;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.therapp.spring.modelo.CategoriaVideos;
+import com.therapp.spring.repositorios.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+    
+    @Autowired
+    CategoriaRepository categoriaRepository;
+
+    public void save(CategoriaVideos cat){
+        categoriaRepository.save(cat);
+    }
+}
