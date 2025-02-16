d6e2987502c9e3fb4a47848271376fb729bca700
didilombi <didlomoca@alu.edu.gva.es>
2025-02-16 17:55:51 +0100

    Filtrado de conversaciones

--- src/main/java/com/therapp/spring/controladores/UsuarioController.java
+++ src/main/java/com/therapp/spring/controladores/UsuarioController.java
@@ -2,13 +2,31 @@ package com.therapp.spring.controladores;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.therapp.spring.dto.CreateUsuarioDTO;
+import com.therapp.spring.dto.PerfilDTO;
+import com.therapp.spring.modelo.ConfirmationToken;
 import com.therapp.spring.modelo.Rol;
 import com.therapp.spring.modelo.Usuario;
 import com.therapp.spring.servicios.EmailService;
@@ -97,6 +115,12 @@ public class UsuarioController {
     @GetMapping("/seguidos-sin-conversacion")
     public ResponseEntity<List<Usuario>> obtenerUsuariosSeguidosSinConversacion(@RequestParam Long usuarioId) {
         List<Usuario> usuarios = usuarioService.obtenerUsuariosSeguidosSinConversacion(usuarioId);
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping("/mas-en-therapp")
+    public ResponseEntity<List<Usuario>> obtenerMasEnTherApp(@RequestParam Long usuarioId) {
+        List<Usuario> usuarios = usuarioService.obtenerMasEnTherApp(usuarioId);
         return ResponseEntity.ok(usuarios);
     }
 }
