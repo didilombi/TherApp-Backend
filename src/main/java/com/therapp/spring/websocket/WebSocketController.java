468cecc40d384a55914f39e323d83930e0ed7e5f
BrathRojo <brath.bolano@iesdoctorbalmis.com>
2025-02-19 01:57:30 +0100

    se supone que el websocket va bien en el back

---
+++ src/main/java/com/therapp/spring/websocket/WebSocketController.java
@@ -0,0 +1,18 @@
+package com.therapp.spring.websocket;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.therapp.spring.dto.MensajeDTO;
+
+@Controller
+public class WebSocketController {
+    @MessageMapping("/chat/{roomId}")
+    @SendTo("/topic/{roomId}")
+    public MensajeDTO chat(@DestinationVariable String roomId, MensajeDTO mensaje) {
+        System.out.println(mensaje);
+        return mensaje;
+    }
+}
